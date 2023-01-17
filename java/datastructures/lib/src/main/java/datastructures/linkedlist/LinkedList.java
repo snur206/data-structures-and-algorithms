fb1421aa072eb7e432b1ad07833e28a1a775cb03
SHARMARKE NUR <Snur206@yahoo.com>
2023-01-17 12:47:08 -0800

    update challenge

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -13,28 +13,37 @@ public class LinkedList {
 
   public void insert(int value) {
     Node tempNode = new Node(value);
-    if (head != null) {
-      tempNode.next = head;
+    if(head != null) {
+      head = tempNode;
+    } else {
+      tempNode.next = this.head;
+      head = tempNode;
     }
-    head = tempNode;
   }
 
   public boolean includes(int value) {
-    if (head == null)
-      return false;
-    Node tempNode = new Node(value);
-    while (tempNode != null) {
-      return true;
+    Node curr = head;
+    while(curr != null); {
+      if(curr.value == value) {
+        return true;
+      } else {
+        curr = curr.next;
+      }
     }
-    tempNode = tempNode.next;
     return false;
   }
-//  return false;
+
 
 
   @Override
   public String toString()
   {
-    return "null";  // TODO: implement me
+    Node curr = head;
+    String outputString = "";
+    while(curr != null) {
+      outputString += "{ " + curr.value + "} -> ";
+    }
+    outputString += "NULL";
+    return outputString;
   }
 }
