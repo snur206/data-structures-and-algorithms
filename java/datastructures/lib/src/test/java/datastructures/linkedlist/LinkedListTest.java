4dbea06679d9eb49e4692f21a00b6ea2d951256f
SHARMARKE NUR <Snur206@yahoo.com>
2023-01-17 20:26:48 -0800

    code challenge 6

--- java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
+++ java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
@@ -2,6 +2,7 @@ package datastructures.linkedlist;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LinkedListTest
@@ -17,4 +18,29 @@ public class LinkedListTest
 //  sut.head = new Node(3);
   sut.insert(3);
 }
+
+@Test void testAppend() {
+  LinkedList sut = new LinkedList();
+  sut.insert(1);
+  sut.insert(2);
+  sut.insert(3);
+  sut.append(5);
+  assertEquals("{1} -> {3} -> {5} -> {2} -> NULL", sut.toString());
+}
+@Test void testInsertBefore() {
+  LinkedList sut = new LinkedList();
+  sut.insert(1);
+  sut.insert(2);
+  sut.insert(4);
+  sut.insertBefore(3, 4);
+  assertEquals("{1} -> {2} -> {3} -> {4} -> NULL", sut.toString());
+  }
+  @Test void testInsertAfter() {
+    LinkedList sut = new LinkedList();
+    sut.insert(1);
+    sut.insert(2);
+    sut.insert(4);
+    sut.insertAfter(3, 4);
+    assertEquals("{1} -> {2} -> {4} -> {3} -> NULL", sut.toString());
+  }
 }
