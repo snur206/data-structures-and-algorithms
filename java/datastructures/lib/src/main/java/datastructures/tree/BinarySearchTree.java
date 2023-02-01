b76a1230e75818c78637d6e9804f9b67ff034547
SHARMARKE NUR <Snur206@yahoo.com>
2023-01-31 20:55:23 -0800

    updated tree cc

--- java/datastructures/lib/src/main/java/datastructures/tree/BinarySearchTree.java
+++ java/datastructures/lib/src/main/java/datastructures/tree/BinarySearchTree.java
@@ -1,17 +1,50 @@
 package datastructures.tree;
 
-import datastructures.tree.Node;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class BinarySearchTree<T extends Comparable<? super T>> extends BinaryTree<T>
-{
-  public void add(T value)
-  {
-    // TODO: implement me
+public class BinarySearchTree<T extends Comparable<? super T>> extends BinaryTree<T>{
+
+
+  public void add(int value){
+    Node<T> node = new Node(value);
+
+    if(this.root == null) {
+      this.root = node;
+    }
+
+    Node<T> current = this.root;
+
+    while(current != null){
+      if((Integer)node.value < (Integer) current.value){
+        if(current.left == null) {
+          current.left = node;
+          return;
+        }
+        current = current.left;
+      } else if ((Integer)node.value > (Integer) current.value){
+        if(current.right == null) {
+          current.right = node;
+          return;
+        }
+        current = current.right;
+      } else {
+        return;
+      }
+    }
   }
 
-  public boolean contains (T value)
-  {
-    // TODO: implement me
+  public boolean contains(int value){
+    Queue<Node<T>> q = new LinkedList<>();
+    if(this.root != null) q.add(this.root);
+
+    while(!q.isEmpty()){
+      Node<T> current = q.poll();
+      if(current.value.equals(value)) return true;
+      if(current.left != null) q.add(current.left);
+      if(current.right != null) q.add(current.right);
+    }
     return false;
   }
+
 }
--- java/datastructures/lib/src/main/java/datastructures/tree/BinaryTree.java
+++ java/datastructures/lib/src/main/java/datastructures/tree/BinaryTree.java
@@ -1,54 +1,68 @@
 package datastructures.tree;
 
 import java.util.ArrayList;
-import datastructures.tree.Node;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class BinaryTree<T>
-{
-  Node <T> root;
-  ArrayList<T> node = new ArrayList<>();
+class BinaryTree <T> {
+  Node<T> root;
 
-  public Object[] preOrderTraversal()
-  {
-    Object[] outputValues = null;
-    // Should return something like "A B C D"
-    // Can use an ArrayList internally
-    // ArrayList.toArray() returns Object[], hence why Object[] is the return type
-    // outputValues = preOrder(root);
-    // TODO: implement me
-    if(root.left != null)
-    preOrderTraversal(root.left);
-    if(root.right != null);
-    preOrderTraversal(root.right);
-    return outputValues;
+  public ArrayList<T> preorder(){
+    ArrayList<T> vals = new ArrayList<>();
+    preorder(root, vals);
+    return vals;
+  }
+  public void preorder(Node<T> current, ArrayList<T> vals){
+    if(current == null) return;
+    vals.add(current.value);
+    preorder(current.left, vals);
+    preorder(current.right, vals);
   }
 
-  public Object[] inOrderTraversal()
-  {
-    Object[] outputValues = null;
-    // outputValues = inOrder(root);
-    // TODO: implement me
-
-    if(root.left != null);
-    inOrderTraversal(root.left);
-    return root.value;
-
-    if(root.right != null);
-    inOrderTraversal(root.right);
-    return outputValues;
+  public ArrayList<T> inOrder(){
+    ArrayList<T> vals = new ArrayList<>();
+    inOrder(root, vals);
+    return vals;
+  }
+  public void inOrder(Node<T> current, ArrayList<T> vals){
+    if(current == null) return;
+    inOrder(current.left, vals);
+    vals.add(current.value);
+    inOrder(current.right, vals);
   }
 
-  public Object[] postOrderTraversal()
-  {
-    Object[] outputValues = null;
-    // outputValues = postOrder(root);
-    // TODO: implement me
-    if(root.left != null);
-    postOrderTraversal(root.left);
-
-    if(root.right != null);
-    postOrderTraversal(root.right);
-
-  return outputValues;
+  public ArrayList<T> postOrder(){
+    ArrayList<T> vals = new ArrayList<>();
+    postOrder(root, vals);
+    return vals;
   }
+  public void postOrder(Node<T> current, ArrayList<T> vals){
+    if(current == null) return;
+    postOrder(current.left, vals);
+    postOrder(current.right, vals);
+    vals.add(current.value);
+  }
+
+  public String toString(){
+    StringBuilder out = new StringBuilder();
+    Queue<Node<T>> q = new LinkedList<>();
+    Queue<Node<T>> q2 = new LinkedList<>();
+    q.add(root);
+
+    while(!q.isEmpty()){
+      Node<T> curr = q.poll();
+      out.append(curr.value + ",");
+      if(curr.left != null) q2.add(curr.left);
+      if(curr.right != null) q2.add(curr.right);
+      if(q.isEmpty()) {
+        q = q2;
+        q2 = new LinkedList<>();
+        out.append("\n");
+      }
+
+    }
+    return out.toString();
+  }
+
+
 }
