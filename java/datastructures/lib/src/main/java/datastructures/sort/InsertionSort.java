6c66cb308fe05631b090520e6d15aaa3ff0d7ff4
SHARMARKE NUR <Snur206@yahoo.com>
2023-02-13 20:02:46 -0800

    cc sort

---
+++ java/datastructures/lib/src/main/java/datastructures/sort/InsertionSort.java
@@ -0,0 +1,15 @@
+package datastructures.sort;
+
+public class InsertionSort(int[] arr) {
+  for(int i = 1; i < arr.length; i++) {
+
+    int j = i - 1;
+    int temp = arr[i];
+
+    while(j >= 0 && temp < arr[j]){
+      arr[j+1] = arr[j];
+      j--;
+    }
+    arr[j+1] = temp;
+  }
+}
