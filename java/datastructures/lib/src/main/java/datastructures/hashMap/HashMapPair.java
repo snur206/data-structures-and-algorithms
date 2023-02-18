49ea82d6e9a02ecd6e8517d8e538887de7e6f930
SHARMARKE NUR <Snur206@yahoo.com>
2023-02-17 18:54:41 -0800

    initial commit

---
+++ java/datastructures/lib/src/main/java/datastructures/hashMap/HashMapPair.java
@@ -0,0 +1,11 @@
+package datastructures.hashMap;
+
+import java.util.AbstractMap;
+
+public class HashMapPair<K, V> extends AbstractMap.SimpleEntry<K, V>
+{
+  public HashMapPair(K key, V value)
+  {
+    super(key, value);
+  }
+}
