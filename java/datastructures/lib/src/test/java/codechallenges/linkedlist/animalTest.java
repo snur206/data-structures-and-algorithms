6ae8eb19e9540b12a1c3de25dd8961a0c05c837a
SHARMARKE NUR <Snur206@yahoo.com>
2023-05-19 21:46:23 -0700

    worked on animal test

--- java/datastructures/lib/src/test/java/codechallenges/linkedlist/animalTest.java
+++ java/datastructures/lib/src/test/java/codechallenges/linkedlist/animalTest.java
@@ -1,4 +1,36 @@
 package codechallenges.linkedlist;
 
+import codechallenges.linkedlist.animal.Animal;
+import codechallenges.linkedlist.animal.AnimalShelter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class animalTest {
+  @Test
+  void enqueueAddsAnimalToShelter() {
+    AnimalShelter sut = new AnimalShelter();
+    Animal dog = new Animal("dog", "cat");
+    sut.enqueue(dog);
+  }
+
+  @Test
+  void dequeueRemovesDogFromShelter() {
+    AnimalShelter sut = new AnimalShelter();
+    Animal dog = new Animal("dog", "Brodie");
+    Animal cat = new Animal("cat", "Simba");
+    sut.enqueue(dog);
+    sut.enqueue(cat);
+    assertEquals("dog",sut.dequeue("dog"));
+  }
+
+  @Test
+  void dequeueRemovesCatFromShelter() {
+    AnimalShelter sut = new AnimalShelter();
+    Animal dog = new Animal("dog", "Brodie");
+    Animal cat = new Animal("cat", "Simba");
+    sut.enqueue(dog);
+    sut.enqueue(cat);
+    assertEquals("cat",sut.dequeue("cat"));
+  }
 }
